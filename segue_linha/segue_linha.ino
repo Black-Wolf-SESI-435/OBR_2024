e75162551e64842b2c121db0c796df75d4cf33b9
Guilherme Meneghin de Souza <guilherme.meneghinsouza@gmail.com>
2024-05-30 21:36:37 -0300

    Adicionei a função de calibragem para o robô.

--- segue_linha/segue_linha.ino
+++ segue_linha/segue_linha.ino
@@ -12,32 +12,96 @@
 
 #define se_vcc 52
 #define sd_vcc 53
+#define botao_calibragem 7
 
-// Constantes:
-#define cruz_cal    100
+
+// Calibragem:
+#define DELAY_CALIBRAGEM 1000
+#define NUM_SENSORES 5
+#define NUM_TESTES 512
+
+#define cruz_cal  100
 #define se_cal    300
 #define sd_cal    200
 
+// Velocidades:
 #define VELOCIDADE_ME 30
 #define VELOCIDADE_MD 30
 
+// Aliases:
 #define GIRAR_DIREITA 0
 #define GIRAR_ESQUERDA 1
 #define NAO_GIRAR 2
 
+long calibragem[NUM_SENSORES];
+
+/*
+ * `calibrar()` usa as cosntantes `NUM_SENSORES` e `NUM_TESTES` para encontrar o
+ *   máximo e mínimo de cada sensor.
+ * Para isso ligue os sensores de forma que eles preencham os pinos analógicos
+ *   em ordem crescende. Com isso, não precisa ser atualizado caso o numero de
+ *   sensores mude.
+ */
+void calibrar() {
+    #if DEBUG
+        Serial.println("Calibrando sensores:");
+        Serial.println("    Colorque os sensores no branco.");
+    #endif
+
+    // Espera pela confirmação do botão.
+    while(digitalRead(botao_calibragem));
+
+    // Calibra o min de cada sensor no branco.
+    for (int sensor = 0; sensor < NUM_SENSORES; sensor++) {
+        calibragem[sensor] = 0;
+        for (int teste = 0; teste < NUM_TESTES; ++teste) {
+            calibragem[sensor] += analogRead(sensor);
+        }
+    }
+
+    #if DEBUG
+        Serial.println("    Colorque os sensores no preto.");
+    #endif
+
+    // Espera pela confirmação do botão.
+    delay(DELAY_CALIBRAGEM);
+    while(digitalRead(botao_calibragem));
+
+    // Calibra o max de cada sensor no preto.
+    for (int sensor = 0; sensor < NUM_SENSORES; sensor++) {
+        for (int teste = 0; teste < NUM_TESTES; ++teste) {
+            calibragem[sensor] += analogRead(sensor);
+        }
+        calibragem[sensor] = calibragem[sensor] / (2*NUM_TESTES);
+    }
+
+    #if DEBUG
+        Serial.println("Fim da Calibragem.");
+        for (int sensor = 0; sensor < NUM_SENSORES; sensor++) {
+            Serial.print(sensor);
+            Serial.print(": ");
+            Serial.println(calibragem[sensor]);
+        }
+    #endif
+    delay(DELAY_CALIBRAGEM);
+}
 
 void setup() {
     #if DEBUG
         Serial.begin(9600);
-        while (!Serial);
     #endif
 
     setup_ponteh();
 
     pinMode(se_vcc, OUTPUT);
     pinMode(sd_vcc, OUTPUT);
+
+    pinMode(botao_calibragem, INPUT_PULLUP);
+
     digitalWrite(se_vcc, HIGH);
     digitalWrite(sd_vcc, HIGH);
+
+    calibrar();
 }
 
 void loop() {
@@ -53,35 +117,34 @@ void loop() {
     #if DEBUG
         Serial.print(val_see);
         Serial.print(" ee ");
-        Serial.print(val_see < cruz_cal);
+        Serial.print(val_see < calibragem[0]);
         Serial.print(", ");
         Serial.print(val_se);
         Serial.print(" e ");
-        Serial.print(val_se > se_cal);
+        Serial.print(val_se > calibragem[1]);
         Serial.print(", ");
         Serial.print(val_sm);
         Serial.print(" m ");
-        Serial.print(val_sm < cruz_cal);
+        Serial.print(val_sm < calibragem[2]);
         Serial.print(", ");
         Serial.print(val_sd);
         Serial.print(" d ");
-        Serial.print(val_sd > sd_cal);
+        Serial.print(val_sd > calibragem[3]);
         Serial.print(", ");
         Serial.print(val_sdd);
         Serial.print(" dd ");
-        Serial.print(val_sdd < cruz_cal);
+        Serial.print(val_sdd < calibragem[4]);
         Serial.println();
     #endif
-    
-    // > calibragem ==> preto: True (1)
-    // < calibragem ==> branco: False (0)
-    val_sd = val_sd > sd_cal;
-    val_se = val_se > se_cal;
-    // < calibragem ==> preto: True (1)
-    // > calibragem ==> branco: False (0)
-    val_sdd = val_sdd < cruz_cal;
-    val_see = val_see < cruz_cal;
-    val_sm = val_sm < cruz_cal;
+
+    // Sensores do cruzamento usam > e sensores da linha usam <
+    // 1 => no preto
+    // 0 => no branco
+    val_see = val_see < calibragem[0];
+    val_se = val_se   > calibragem[1];
+    val_sm = val_sm   < calibragem[2];
+    val_sd = val_sd   > calibragem[3];
+    val_sdd = val_sdd < calibragem[4];
 
 
     #if CRUZAMENTO
@@ -107,9 +170,6 @@ void loop() {
             int caminho = (val_see<<2) + (val_sm<<1) + val_sdd;
 
             switch (caminho) {
-                case 0b000:
-                    while (true);
-                    break;
                 case 0b100:
                     me_ligar(-VELOCIDADE_ME);
                     md_ligar(VELOCIDADE_MD);
